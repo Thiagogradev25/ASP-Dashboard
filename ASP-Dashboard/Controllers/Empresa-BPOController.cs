47378007397d4e87903b181250c9eb13a5b34642
Thiagogradev25 <98544300+Thiagogradev25@users.noreply.github.com>
2022-04-25 09:57:36 -0300

    ok

---
+++ ASP-Dashboard/Controllers/Empresa-BPOController.cs
@@ -0,0 +1,26 @@
+﻿using ASP_Dashboard.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP_Dashboard.Controllers;
+
+public class Empresa_BPO : Controller
+{
+
+    private readonly ILogger<Empresa_BPO> _logger;
+
+    public Empresa_BPO(ILogger<Empresa_BPO> logger)
+    {
+        _logger = logger;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ ASP-Dashboard/Controllers/ItemMenuController.cs
@@ -0,0 +1,26 @@
+﻿using ASP_Dashboard.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Dashboard.Controllers
+{
+    public class ItemMenuController : Controller
+    {
+
+   
+        private readonly IItemMenuRepository _ItemMenuRepository;
+
+        public ItemMenuController(IItemMenuRepository itemMenuRepository)
+        {
+            _ItemMenuRepository = itemMenuRepository;
+        }
+
+        public IActionResult List()
+        {
+            var ItemsMenu = _ItemMenuRepository.ItemsMenu;
+            return View(ItemsMenu);
+        }
+
+
+
+    }
+}
