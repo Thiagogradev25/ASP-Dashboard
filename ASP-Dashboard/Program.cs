8dea5c693e49a0a6cac0e98b3cd0e5b26c32a5f1
Thiagogradev25 <98544300+Thiagogradev25@users.noreply.github.com>
2022-04-25 13:17:32 -0300

    Update Program.cs

--- ASP-Dashboard/Program.cs
+++ ASP-Dashboard/Program.cs
@@ -1,12 +1,14 @@
+using ASP_Dashboard_Server.Repositories;
+using ASP_Dashboard_Server.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
-//builder.Configuration.GetConnectionString("DefaultConnection");
+
 
 // Add services to the container.
 
-//builder.Services.AddDbContextFactory<AppDbContext>(options =>
-//   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-//builder.Services.AddTransient<IItemMenuRepository, ItemMenuRepository>();
+
+builder.Services.AddTransient<IItemMenuRepository, ItemMenuRepository>();
 
 builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
@@ -18,8 +20,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+   
     app.UseHsts();
 }
 
@@ -32,19 +33,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
 
-    endpoints.MapControllerRoute(
-     name: "default",
-     pattern: "{controller=ItemMenu}/{action=List}/{id?}");
-
-    //endpoints.MapControllerRoute(
-    //name: "menu",
-    //pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
-});
 
 
 app.Run();
